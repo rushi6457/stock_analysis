b21c1bbfafe56ef7541712a5e3d4b8740ee46f59
Rushikesh Patil <103261302+rushi6457@users.noreply.github.com>
2025-08-23 19:35:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+os.environ["STREAMLIT_WATCHDOG_DISABLE"] = "true"
 import pandas as pd
 from select_top30 import load_consolidated_data, rank_with_forecast, backtest_selector, detect_and_read_raw, find_header_row_with_column, read_with_detected_header
 
